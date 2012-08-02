6db1f00f6fd4615ae94a8ba416eb6fb49036bbad
SaschaNaz_cp <SND\SaschaNaz_cp>
2012-08-02 11:37:59 +0000

    아래 추가 (...

---
+++ NemoKachi/AccountTokenCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemoKachi
+{
+    public class AccountTokenCollector
+    {
+        public ObservableCollection<TwitterWrapper.AccountToken> TokenCollection { get; private set; }
+
+        public AccountTokenCollector()
+        {
+            TokenCollection = new ObservableCollection<TwitterWrapper.AccountToken>();
+        }
+
+        public TwitterWrapper.AccountToken GetTokenByID(UInt64 Id)
+        {
+            foreach (TwitterWrapper.AccountToken aToken in TokenCollection)
+                if (aToken.AccountId == Id)
+                    return aToken;
+            return null;
+        }
+    }
+}
